a4a35b9247e9e41f48725ede20638411a3f75ca7
赵杰 <zhaojay1211@gmail.com>
2025-03-15 13:26:36 +0800

    feat: finish chapter 6

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,8 +1,19 @@
+'use client';
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+/* 
+Link 按 路由拆分代码，在传统的SPA应用中，浏览器在页面初始化的时候会加载所有的代码。
+按 路由拆分代码 就是按需加载，访问 /dashborad 页面就只加载 dashboard 相关的代码
+如果页面中有 Link 文件就会，next.js 会在后台自动预取链接路由的代码。
+*/
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -17,19 +28,26 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,11 @@
 import { Inter, Lusitana } from "next/font/google";
 
+// 在网站中，添加字体，可以在这里配置
+/* 
+为什么要配置字体？
+- 累积布局偏移，是评估网站体验和性能优化的指标
+- 我们使用next/font的时候，next.js 会在构建的时候，下载字体和静态资源一起托管，就不会有其他的网络请求，来请求字体
+*/
 export const inter = Inter({ subsets: ["latin"] });
 
 export const lusitana = Lusitana({
