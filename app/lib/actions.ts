69253bc37ff9a6ffd42b58c75f1c9e80c0c3695c
赵杰 <zhaojay1211@gmail.com>
2025-03-16 15:53:01 +0800

    fix: eslint error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -80,7 +80,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
-  } catch (error) {
+  } catch (_error) {
     // If a database error occurs, return a more specific error.
     return {
       message: "Database Error: Failed to Create Invoice.",
@@ -121,7 +121,7 @@ export async function updateInvoice(
       SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
       WHERE id = ${id}
     `;
-  } catch (error) {
+  } catch (_error) {
     return { message: "Database Error: Failed to Update Invoice." };
   }
 
